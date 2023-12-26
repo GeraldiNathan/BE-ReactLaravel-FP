16ef98f9f5300f9c740e16223e422920d9865d4c
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-26 08:20:10 +0800

    WIP (API) : add response for create and view api

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,22 +4,59 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class PostController extends Controller
 {
-    public function index(){
-        return Post::all();
+    public function index()
+    {
+        $data = Post::all();
+        if ($data->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "Data empty"
+            ], 404);
+        }
     }
 
-    public function addPost(Request $request){
-        
+
+    public function show($id)
+    {
+    }
+
+    public function addPost(Request $request)
+    {
+
         $post = new Post();
         $post->title = $request->input('title');
         $post->description = $request->input('description');
         $post->file_path = $request->file('file_path')->store('public');
         $post->save();
 
-        return $post;
+        if ($post) {
+            return response()->json([
+                'status' => 200,
+                'message' => "add data successfully done",
+                'data' => $post
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'message' => "something went wrong"
+            ], 404);
+        };
+    }
 
+    public function edit()
+    {
+    }
+
+    public function destroy()
+    {
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,5 +10,4 @@ class Post extends Model
     use HasFactory;
     protected $table = 'posts';
     protected $fillable = ['title', 'description', 'file_path'];
-
 }
